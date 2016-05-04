ff4fb85076d0ce36cd57b1d713bd67a485329077
Harry S <haris.strong@gmail.com>
2016-05-04 19:06:12 +0100

    checkpoint

--- src/main/java/com/cs391/service/DbUserRegister.java
+++ src/main/java/com/cs391/service/DbUserRegister.java
@@ -22,32 +22,38 @@ public class DbUserRegister {
     
     @PostConstruct
     private void register() {
-        try {
-            MessageDigest md = MessageDigest.getInstance("SHA-256");
-            md.update("admin1".getBytes("UTF-8"));
-            byte[] digest = md.digest();
-            BigInteger bigInt = new BigInteger(1, digest);
-            String paswdToStoreInDB = bigInt.toString(16);
-
-            Administrator admin = new Administrator();
-            admin.setSussexID("admin1");
-            admin.setName("admin");
-            admin.setSurname("admin");
-            admin.setEmail("admin1@sussex.ac.uk");
-            admin.setPhoneNum("0123456789");
-            em.persist(admin);
-            
-            Credentials credentials = new Credentials();
-            credentials.setSussexID("admin1");
-            credentials.setPass(paswdToStoreInDB);
-            em.persist(credentials);
-
-            UserGroup userGroup = new UserGroup();
-            userGroup.setSussexID("admin1");
-            userGroup.setGroupName("administrator");
-            em.persist(userGroup);
-        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
+//        try {
+//            try{
+//                em.remove(em.merge(em.createQuery("SELECT a FROM Administrator a").getResultList()));
+//                em.remove(em.merge(em.createQuery("SELECT c FROM Credentials c").getResultList()));
+//                em.remove(em.merge(em.createQuery("SELECT u FROM UserGroup u").getResultList()));
+//            } catch (Exception e){}
+//            
+//            MessageDigest md = MessageDigest.getInstance("SHA-256");
+//            md.update("admin1".getBytes("UTF-8"));
+//            byte[] digest = md.digest();
+//            BigInteger bigInt = new BigInteger(1, digest);
+//            String paswdToStoreInDB = bigInt.toString(16);
+//
+//            Administrator admin = new Administrator();
+//            admin.setSussexID("admin1");
+//            admin.setName("admin");
+//            admin.setSurname("admin");
+//            admin.setEmail("admin1@sussex.ac.uk");
+//            admin.setPhoneNum("0123456789");
+//            em.persist(admin);
+//            
+//            Credentials credentials = new Credentials();
+//            credentials.setSussexID("admin1");
+//            credentials.setPass(paswdToStoreInDB);
+//            em.persist(credentials);
+//
+//            UserGroup userGroup = new UserGroup();
+//            userGroup.setSussexID("admin1");
+//            userGroup.setGroupName("administrator");
+//            em.persist(userGroup);
+//        } catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+//            System.out.println("Failed to register new user");
+//        }
     }
 }
